0cd05e30b122b6709e5a304bab3381285946d9ae
Daniel Hernandez <105814072+Dxniel7@users.noreply.github.com>
2023-10-08 22:32:23 -0600

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,1 +1,11 @@
+from TipoToken import TipoToken 
+
+class Token:
+  def __init__(self, tipo, lexema, literal=None): 
+    self.tipo = tipo
+        self.lexema = lexema
+        self.literal = literal
+
+    def __str__(self):
+        return f"<{self.tipo} {self.lexema} {self.literal}>"
 
