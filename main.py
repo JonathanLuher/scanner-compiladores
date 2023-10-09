b69da6c18bc09aae60d3e63824ba995c8db965e3
Daniel Hernandez <105814072+Dxniel7@users.noreply.github.com>
2023-10-08 22:38:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from Scanner import Scanner
+import sys
+
+def ejecutar_archivo(path):
+    try:
+        with open(path, "r") as archivo:
+            source = archivo.read()
+            scanner = Scanner(source)
+            tokens = scanner.scan()
+
+            for token in tokens:
+                print(token)
+    except FileNotFoundError:
+        print(f"No se pudo encontrar el archivo '{path}'")
+
+def main():
+    existen_errores = False
+
+    if len(sys.argv) != 1:
+        print("Uso correcto: Interprete")
+        sys.exit(64)
+
+    ejecutar_archivo("Pruebas.txt")
+
+if __name__ == "__main__":
+    main()
 
